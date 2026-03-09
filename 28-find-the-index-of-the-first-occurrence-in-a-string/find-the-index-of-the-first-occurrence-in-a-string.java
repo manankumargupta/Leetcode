a3a7da5ddf87d35024a3546b14d1654dbcca7ea7
Manan Gupta <manangupta200505@gmail.com>
2026-03-09 11:05:38 +0530

    Time: 2 ms (15.15%) | Memory: 43.1 MB (51.09%) - LeetSync

--- 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
+++ 28-find-the-index-of-the-first-occurrence-in-a-string/find-the-index-of-the-first-occurrence-in-a-string.java
@@ -1,34 +1,31 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        if (needle.isEmpty()) {
+        if(needle.isEmpty()) {
             return 0;
         }
 
         int haystackLength = haystack.length();
         int needleLength = needle.length();
-      
-        int haystackPointer = 0;  
-        int needlePointer = 0;  
-      
-        while (haystackPointer < haystackLength) {
-            if (haystack.charAt(haystackPointer) == needle.charAt(needlePointer)) {
-                if (needleLength == 1) {
+
+        int haystackPointer = 0;
+        int needlePointer = 0;
+
+        while(haystackPointer < haystackLength) {
+            if(haystack.charAt(haystackPointer) == needle.charAt(needlePointer)) {
+                if(needleLength == 1) {
                     return haystackPointer;
                 }
-              
+
                 haystackPointer++;
                 needlePointer++;
             } else {
-                haystackPointer = haystackPointer - needlePointer + 1;
-              
-                needlePointer = 0;
+            haystackPointer = haystackPointer - needlePointer + 1;
+            needlePointer = 0;
             }
-
-            if (needlePointer == needleLength) {
-                return haystackPointer - needlePointer;
-            }
+            if(needlePointer == needleLength) {
+            return haystackPointer - needlePointer;
         }
-      
-        return -1;
+    }
+    return -1;
     }
 }
